11241bb9fc94b44a28c61a66bf2251ddb2edc72d
arthurfoschiani <arthur.foschiani@outlook.com>
2022-09-13 21:21:35 -0300

    Até 50

---
+++ src/entrada_processamento_saida/Ex49.java
@@ -0,0 +1,29 @@
+package entrada_processamento_saida;
+
+import java.util.Scanner;
+
+public class Ex49 {
+
+	public static void main(String[] args) {		
+		Scanner ler = new Scanner(System.in);
+		
+		int num1, num2, soma = 0;
+		
+		System.out.printf("Digite um número: ");
+		num1 = ler.nextInt();
+		
+		System.out.printf("Digite um número maior: ");
+		num2 = ler.nextInt();
+		
+		while (num1 >= num2) {
+			System.out.printf("Valor inválido. Digite novamente um número maior: ");
+			num2 = ler.nextInt();
+		}
+
+		for (int i = num1; i <= num2; i++) {
+		    soma += i;
+		}
+		
+		System.out.print(soma);
+	}
+}
